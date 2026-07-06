e4a9b274b405e049b17acd545a9b413767359515
Sam Antony <samantony@ronsystech.com>
2026-07-06 22:33:44 +0530

    70%commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 
 export default defineConfig({
+  site: "https://saman2ny.github.io",
+  output: "static",
+
   integrations: [react()],
+
   server: {
-    host: true
-  }
+    host: true,
+  },
 });
